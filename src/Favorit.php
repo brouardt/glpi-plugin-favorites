419b2f4add0f3e25393d65e8ad3b66576e704abc
Thierry Brouard <thierry.brouard@sevengroup.fr>
2026-06-12 15:24:51 +0200

    rename

--- src/Favorit.php
+++ src/Favorit.php
@@ -15,11 +15,28 @@ class Favorit extends \CommonDBTM
         return _n('Favorit', 'Favorites', $nb, 'favorites');
     }
 
+    public static function getMenuName()
+    {
+        return __s('Favorite plugin');
+    }
     /**
      * @return string
      */
     static function getIcon()
     {
         return "ti ti-heart";
+    }
+
+    public static function getAdditionalMenuLinks()
+    {
+        global $CFG_GLPI;
+
+        $links = [];
+
+        $links['config'] = '/plugins/favorite/index.php';
+        $links["<img  src='" . $CFG_GLPI['root_doc'] . "/pics/menu_showall.png' title='" . __s('Show all') . "' alt='" . __s('Show all') . "'>"] = '/plugins/favorite/index.php';
+        $links[__s('Test link', 'example')] = '/plugins/favorite/index.php';
+
+        return $links;
     }
 }
